10e04dd674442f3f16021a544ef394dfc59ff8ac
Aditya Sood <adityasood314@gmail.com>
2023-06-05 12:37:37 +0530

    Notes RecyclerView created.

--- app/src/main/java/com/example/notemaster/mainFragment.kt
+++ app/src/main/java/com/example/notemaster/mainFragment.kt
@@ -5,20 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.notemaster.databinding.FragmentMainBinding
+import com.example.notemaster.models.NoteResponse
+import com.example.notemaster.utils.NetworkResult
+import com.google.gson.Gson
 
 class mainFragment : Fragment() {
     private var _binding:FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+    private val noteViewModel by viewModels<NoteViewModel>()
+    private lateinit var noteAdapter: NoteAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(inflater,container,false)
-
+        noteAdapter = NoteAdapter(::onNoteClicked)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindObservers()
+        noteViewModel.getAllNotes()
+        binding.noteList.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.noteList.adapter = noteAdapter
+    }
+
+    private fun bindObservers() {
+        noteViewModel.notesLiveData.observe(viewLifecycleOwner, Observer {
+            when(it){
+                is NetworkResult.Success ->{
+                    noteAdapter.submitList(it.data)
+                }
+                is NetworkResult.Error -> {
+                    Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                }
+                is NetworkResult.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
+    private fun onNoteClicked(noteResponse: NoteResponse){
+        val bundle = Bundle()
+        bundle.putString("note", Gson().toJson(noteResponse))
+        findNavController().navigate(R.id.action_mainFragment_to_noteFragment)
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
